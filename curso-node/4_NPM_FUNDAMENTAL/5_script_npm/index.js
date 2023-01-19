823239585c1b2365a4a47330665c0b47f44e0723
Gabriel <gabrielpierrout@outlook.com.br>
2023-01-19 00:27:32 -0300

    iniciado módulo de fundamentos do npm

---
+++ curso-node/4_NPM_FUNDAMENTAL/5_script_npm/index.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+const chalk = require('chalk')
+
+const a = [1, 2, 3, 4, 5]
+const b = [2, 4, 6, 7, 8]
+
+const diff = _.difference(a,b)
+
+console.log(chalk.bgRed.bold(diff))
+
+// npx npm-check-updates -u => Utilizado para checar updates em pacotes.
+
