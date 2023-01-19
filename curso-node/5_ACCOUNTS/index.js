9ab53b7a2bd3c3943a38d88f5bde5e8c17a9102f
Gabriel <gabrielpierrout@outlook.com.br>
2023-01-19 14:52:27 -0300

    iniciando projeto 1: accounts

---
+++ curso-node/5_ACCOUNTS/index.js
@@ -0,0 +1,41 @@
+// módulos externos
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+// módulos internos
+const fs = require('fs')
+
+operation()
+
+function operation() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'Olá, O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair',
+            ],        
+        },
+    ])
+    .then((answer => {
+
+        const action = answer['action']
+
+        if (action === 'Criar Conta') {
+            createAccount()
+        }
+
+    }))
+    .catch(err => console.log(err))
+}
+
+// create an account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
