1ab22c06a5713da6aaa45e3adb6398d45a9b29da
Gabriel <gabrielpierrout@outlook.com.br>
2023-01-20 22:56:06 -0300

    adicionado função criar conta e encerrar programa

--- curso-node/5_ACCOUNTS/index.js
+++ curso-node/5_ACCOUNTS/index.js
@@ -28,6 +28,15 @@ function operation() {
 
         if (action === 'Criar Conta') {
             createAccount()
+        } else if (action === 'Depositar') {
+            
+        } else if (action === 'Consultar Saldo') {
+
+        } else if (action === 'Sacar') {
+
+        } else if (action === 'Sair') {
+            console.log(chalk.bgBlue.black('Obrigado por usar o Account!'))
+            process.exit()
         }
 
     }))
@@ -38,4 +47,43 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+
+    inquirer.prompt([
+        {
+        name: 'accountName',
+        message: 'Digite um nome para sua conta',
+        },
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName']
+
+        console.info(answer['accountName'])
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'),
+            )
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}',
+            function (err) {
+                console.log(err)
+            },
+        )
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+    })
+    .catch((err) => console.log(err))
+
 }
