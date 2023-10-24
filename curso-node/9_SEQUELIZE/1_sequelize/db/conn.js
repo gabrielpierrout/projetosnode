9aa9ff2e1c0785a49ee4c28408c45245b5893f4b
Gabriel Pierrout <gabrielpierrout@outlook.com.br>
2023-10-23 21:41:06 -0300

    iniciando utilização do sequelize com nodejs

---
+++ curso-node/9_SEQUELIZE/1_sequelize/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+
+    sequelize.authenticate()
+    console.log('Conectado com sucesso ao Sequelize!')
+
+} catch(err) {
+    console.log('Não foi possível conectar: ', error)
+}
+
+module.exports = sequelize
